872d0eeacd7dfdc7770f50870e6cded9667a2be8
pitr77 <p.kalavsky@gmail.com>
2026-04-26 09:25:22 +0200

    Revert portfolio layout changes

--- views/portfolio-detail.php
+++ views/portfolio-detail.php
@@ -12,25 +12,21 @@ $dir = __DIR__ . '/../assets/img/' . $category;
 if (is_dir($dir)) {
     $files = glob($dir . '/*.{jpg,jpeg,png,JPG,JPEG,PNG}', GLOB_BRACE);
     foreach ($files as $file) {
-        // Získame rozmery obrázka pre výpočet aspect-ratio
-        $size = @getimagesize($file);
-        $photos[] = [
-            'url' => '/assets/img/' . $category . '/' . basename($file),
-            'width' => $size ? $size[0] : 800,
-            'height' => $size ? $size[1] : 1000
-        ];
+        $photos[] = '/assets/img/' . $category . '/' . basename($file);
     }
 }
 
-// SIMULÁCIA: Zduplikujeme fotky
+// SIMULÁCIA: Zduplikujeme fotky a náhodne ich zamiešame, aby sme simulovali väčšiu galériu
 $all_photos = [];
 if (!empty($photos)) {
+    // Skopírujeme ich 4x, aby sme ich mali dostatok na ukážku
     for ($i = 0; $i < 4; $i++) {
         $all_photos = array_merge($all_photos, $photos);
     }
-    // Pre zachovanie poradia na mobile (aby neboli rozhádzané)
-    // shuffle($all_photos); // Ak chceš presné poradie, vypni shuffle
+    // Zamiešame poradie
+    shuffle($all_photos);
 }
+
 
 
 // Zmena hlavičky pre podstránky (aby nebola transparentná nad obrázkami ako na domovskej)
@@ -45,12 +41,13 @@ require __DIR__ . '/layout/header.php';
     <a href="/" class="floating-back-btn">&larr; Späť na domov</a>
     
                 <section class="masonry-grid-full">
-        <?php foreach ($all_photos as $index => $photo): ?>
-        <div class="masonry-item-full portfolio-img" style="aspect-ratio: <?php echo $photo['width']; ?> / <?php echo $photo['height']; ?>; background-color: #1a1a1a;">
-            <img src="<?php echo $photo['url']; ?>" alt="Fotografia portfólia <?php echo $index; ?>" loading="lazy" data-src="<?php echo $photo['url']; ?>">
-        </div>
-        <?php endforeach; ?>
+                <?php foreach ($all_photos as $index => $photo): ?>
+                <div class="masonry-item-full portfolio-img">
+                    <img src="<?php echo $photo; ?>" alt="Fotografia portfólia <?php echo $index; ?>" loading="lazy" data-src="<?php echo $photo; ?>">
+                </div>
+                <?php endforeach; ?>
     </section>
+
 
 </div>
 
